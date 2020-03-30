11c67134b2978199e0c29e0b62dcda4554c7d729
019252752 <62552438+019252752@users.noreply.github.com>
2020-03-30 09:47:14 +0200

    Update tablica.h
    Proba poprawy tablicy przy problemach z ubuntu i nauki wektorów z pomocą

--- lab1/tablica.h
+++ lab1/tablica.h
@@ -1,6 +1,59 @@
-#ifndef TABLICA_H
-#define TABLICA_H
+#ifndef tablica_h
+#define tablica_h
 
-	void cell_update(){
+ #include <iostream>
+  #include <vector>
+
+using namespace std;
+
+  struct tab
+{
+	int a;
+	int b;
+}Tab;
+
+class Table
+{
+	public:
+	static void changeSize(vector<vector<int>> table, int new_width, int new_height);
+	static void changeData(vector<vector<int>> table, int xPos, int yPos, int new_data);
+	static void display(vector<vector<int>> table);
+};
+
+   void Table::display(vector<vector<int>> table)
+{
+	for(int i = 0; i<table.size(); i++)
+	{
+		for(int j = 0; j<table[i].size(); j++)
+		{
+			std::cout << table[i][j];
+			std::cout << " ";
+		}
+		std::cout << std::endl;
 	}
+
+}
+
+   void Table::changeData(vector<vector<int>> table, int aPos , int bPos, int new_data)
+{
+	if (table.size() > 0)
+	{
+		if(bPos>table.size() || aPos > table[0].size())
+		{
+			cout << "Niepoprawna wartosc" << sendl;
+		} else{
+			table.at(bPos).at(aPos) = new_data;
+		}
+	}
+}
+
+  void Table::changeSize(vector<vector<int>> table, int new_width, int new_height)
+{
+	table.resize(new_height);
+
+	for(int i =0; i<table.size(); i++)
+	{
+		table[i].resize(new_width);
+	}
+}
 #endif
