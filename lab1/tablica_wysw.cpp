c788af37a0debe3341b414e189bb8cc653106f7a
019252752 <62552438+019252752@users.noreply.github.com>
2020-03-30 09:49:15 +0200

    Update tablica_wysw.cpp
    Proba poprawy tablica_wysw przy problemach z ubuntu

--- lab1/tablica_wysw.cpp
+++ lab1/tablica_wysw.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
-
+#include <stdlib.h>
+#include <vector>
+#include "tablica.h"
+ 
 using namespace std;
 
-int main(){
-return 0;
+ int main(){
+	vector<vector<int>> table{{1,2,3},
+		                            {1,2,3},
+					    {1,2,3}};
+	 Table::changeData(table,0,0,100);
+	 Table::display(table);
+	 return 0;
 }
