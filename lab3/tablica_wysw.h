eb0c9b4fdb2514c43bab3bdd943220b8ff01c65c
019252752 <62552438+019252752@users.noreply.github.com>
2020-05-18 12:12:18 +0200

    Update tablica_wysw.h

---
+++ lab3/tablica_wysw.h
@@ -0,0 +1,14 @@
+#ifndef tablicawysw_hpp_
+#define tablicawysw_hpp_
+#include <iostream>
+#include <fstream>
+
+/**
+* @param[in] Tablica tab - struktura 
+* @param[out] tab.tablica_str[x][y] - elementy w tablicy
+*/
+
+void wyswietlanie(Tablica & tab);
+
+
+#endif
