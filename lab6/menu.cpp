ea9a1404d87557ce944e0e55adb58aaf07c3a243
019252752 <252752@student.pwr.edu.pl>
2020-06-12 06:39:15 -0400

    male zmiany

--- lab6/menu.cpp
+++ lab6/menu.cpp
@@ -6,12 +6,12 @@ int menu(){
 
 	int m=0;	
 	cout << endl <<"MENU:" << endl;
-	cout << "1. Stworz nowa tabele (poprzednia zostanie usunieta)" << endl <<
-		"2. Zmien rozmiar istniejacej tabeli" << endl <<
-		"3. Aktualizuj istniejace elementy w tabeli" << endl <<
-		"4. Wyswietl zawartosc tabeli" << endl
-		<< "5. Operacje matematyczne" << endl
-		<< "6. Zakoncz program" << endl;
+	cout << "1. Stworz nowa tabele (usun poprzednia)." << endl;
+	cout << "2. Zmien rozmiar." << endl;
+	cout << "3. Aktualizuj elementy tabeli." << endl;
+	cout << "4. Wyswietl tabele." << endl;
+	cout << "5. Operacje matematyczne" << endl;
+	cout << "6. Zakoncz program" << endl;
 	cin >> m;
 					
 	return m;
@@ -20,11 +20,11 @@ int menu(){
 int typeMenu(){
 	int t=0;
 		
-	cout << "Chcesz stworzyc i wczytac arkusz z liczbami czy slowami?"<< endl;
-	cout << "1. Liczbami" << endl << "2. Slowami" << endl;
+	cout << "Stworz/wczytaj arkusz z:"<< endl;
+	cout << "1. Liczbami." << endl << "2. Slowami." << endl;
 		
 	while(t!=1 && t!=2){
-		cout << "Wcisnij 1 lub 2";
+		cout << "Wybierz 1 lub 2." << endl;
 		cin >> t;
 	}
 		
@@ -33,18 +33,18 @@ int typeMenu(){
 	
 void interface_type_choose(){
 		
-	cout << endl <<"Jezeli chcesz zeby kolumna o numerze podanym wyzej byla liczbowa, wcisnij 0. Jezeli chcesz zeby byla tekstowa, wcisnij 1." << endl;		
+	cout << endl <<"Dla kolumny liczbowej wybierz 0, a dla tekstowej wybierz 1." << endl;		
 }
 
 int menu_operations(){
 	
 	int m;
-	cout << "1. Zsumuj elementy w wybranym wierszu." << endl 
-		<< "2. Zsumuj elementy w wybranej kolumnie."<< endl
-		<< "3. Znajdz najmniejszy element" << endl
-		<< "4. Znajdz najwiekszy element" << endl 
-		<<  "5. Policz srednia" << endl
-		<< "Wcisnij 6 zeby wrocic do menu" << endl;
+	cout << "1. Zsumuj elementy w wybranym wierszu." << endl; 
+	cout << "2. Zsumuj elementy w wybranej kolumnie."<< endl;
+	cout << "3. Znajdz najmniejszy element" << endl;
+	cout << "4. Znajdz najwiekszy element" << endl ;
+	cout << "5. Policz srednia" << endl;
+	cout << "6. Wroc do menu" << endl;
 	cin >> m;
 	
 	return m;
@@ -54,10 +54,10 @@ void interface(int x){
 			
 	switch(x){
 		case 1: 
-			cout << "FILES ERROR";
+			cout << "FILES ERROR" << endl;
 			break;
 		case 2:
-			cout << endl << "Nowe wymiary musza byc wieksze od poprzednich!" << endl << "ERROR";
+			cout << endl << "Nowe wymiary musza byc wieksze od starych." << endl << "FATAL ERROR" << endl;
 			break;
 		default:
 			break;
@@ -68,26 +68,26 @@ void interface_aktualizowanie(int x){
 	
 	switch(x){
 		case 1:
-			cout << "Rozmiar obecnej tablicy to: " ;
+			cout << "Rozmiar tablicy:" << endl;
 			break;
 		case 2:
-			cout << "Ktory element tablicy chcesz edytowac?" << endl;
-			cout << "Wpisz numer wiersza: ";
+			cout << "Wybierz element do zmiany." << endl;
+			cout << "Podaj numer wiersza:" << endl;
 			break;
 		case 3:
-			cout << "Taki element nie istnieje, sprobuj ponownie: ";
+			cout << "Nieprawidlowy element, sprobuj jeszcze raz:" << endl;
 			break;
 		case 4:
-			cout << "Wpisz numer kolumny: ";
+			cout << "Podaj numer kolumny:" << endl;
 			break;
 		case 5:
-			cout << "W tej komorce znajduje sie: ";
+			cout << "Zawartosc komorki:" << endl;
 			break;
 		case 6:
-			cout << "Wprowadz nowa wartosc: ";
+			cout << "Nowa wartosc:" << endl;
 			break;
 		case 7:
-			cout << endl << "Czy chcesz zakonczyc edycje? Jezeli tak wcisnij 1, jezeli nie kliknij dowolny inny przycisk. ";
+			cout << endl << "Wybierz 1, by zakonczyc zmiany lub inny przycisk, by kontynuowac." << endl;
 			break;
 	}
 }
@@ -96,10 +96,10 @@ void interface_how(int x){
 
 	switch(x){
 		case 1:
-			cout << "Podaj ilosc wierszy: ";
+			cout << "Podaj ilosc wierszy:" << endl;
 			break;
 		case 2:
-			cout << "Podaj ilosc kolumn: ";
+			cout << "Podaj ilosc kolumn: " << endl;
 			break;
 		case 3:
 			cout << endl << "Wprowadzono bledno wartosc, wpisz poprawna jeszcze raz." <<  endl;
@@ -111,13 +111,13 @@ void interface_wysw(int x){
 
 	switch(x){
 		case 1:
-			cout << endl << "Kolumna: ";
+			cout << endl << "Kolumna:" << endl;
 			break;
 		case 2:
-			cout << "Wprowadz wspolrzedne:" << endl << "Wiersz: ";
+			cout << "Podaj wspolrzedne:" << endl << "Wiersz:";
 			break;
 		case 3:
-			cout << endl << "Nie ma takiej wspolrzednej, wprowadz poprawna: ";
+			cout << endl << "Nieprawidlowa wspolrzedna, sprobuj ponownie:";
 			break;
 	}
 }
@@ -126,14 +126,14 @@ void interface_add(int x){
 
 	switch(x){
 		case 1:
-			cout << endl << "Ktory wiersz chcesz zsumowac?";
-			cout << endl << "Nr wiersza: ";
+			cout << endl << "Wierz do zsumowania.";
+			cout << endl << "Nr wiersza:";
 			break;
 		case 2:
-			cout <<endl <<" Blad, sprobuj  ponownie: ";
+			cout <<endl << "Blad, sprobuj ponownie:";
 			break;
 		case 3:
-			cout << endl <<"Suma: ";
+			cout << endl << "Suma:";
 			break;
 		case 4:
 			cout << endl << "Ktora kolumne chcesz zsumowac?";
@@ -146,13 +146,13 @@ void interface_min(int x){
 
 	switch(x){
 		case 1:
-			cout << "Jezeli chcesz znalezc minimum w wybranym wierszu, wcisnij 1." << endl <<"Jezeli chcesz znalezc minimum w wybranej kolumnie, wcisnij 2";
+			cout << "Dla minimum w wierzu wybierz 1." << endl <<"Dla minimum w kolumnie wybierz 2" << endl;
 			break;
 		case 2:
-			cout << "Najmniejszy elelement to: ";
+			cout << "Najmniejszy elelement:" << endl;
 			break;
 		case 3:
-			cout << endl << "Wpisz numer: ";
+			cout << endl << "Wpisz numer:" << endl;
 			break;
 	}
 }
@@ -161,10 +161,10 @@ void interface_max(int x){
 
 	switch(x){
 		case 1:
-			cout << "Jezeli chcesz znalezc maximum w wybranym wierszu, wcisnij 1." << endl <<"Jezeli chcesz znalezc maximum w wybranej kolumnie, wcisnij 2";
+			cout << "Dla maximum w wierszu wybierz 1." << endl <<"Dla maximum w kolumnie wybierz 2." << endl;
 			break;
 		case 2:
-			cout << "Najwiekszy elelement to: ";
+			cout << "Najwiekszy elelement to:" << endl;
 			break;
 	}
 }
@@ -173,13 +173,13 @@ void interface_av(int x){
 
 	switch(x){
 		case 1:
-			cout << "Jezeli chcesz wyliczyc srednia w wybranym wierszu wcisnij 1. Jezeli chcesz wyliczyc srednia w wybranej kolumnie wcisnij 2." << endl;
+			cout << "Dla sredniej w wierszu wybierz 1. Dla sredniej w kolumnie wybierz 2." << endl;
 			break;
 		case 2:
-			cout << "Srednia wszystkich elementow wynosi: ";
+			cout << "Srednia wszystkich elementow:" << endl;
 			break;
 		case 3:
-			cout << endl << "Wpisz numer: ";
+			cout << endl << "Wpisz numer:" << endl;
 			break;
 	}
 	
@@ -188,6 +188,6 @@ void interface_av(int x){
 void operations_check(int x){
 	
 	if(x==0){
-		cout << endl << "W czasie przeprowadzania dzialania wystapil blad (nie mozna zsumowac komorek zawierajacych litery)" << endl;
+		cout << endl << "Wystapil blad podczas dzialania." << endl;
 	}
 }
