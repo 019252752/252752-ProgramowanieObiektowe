ad48014a56f9c063a9be343470df03315abc9478
019252752 <62552438+019252752@users.noreply.github.com>
2020-06-15 13:54:03 +0200

    Update Table_view.cpp

--- lab6/Table_view.cpp
+++ lab6/Table_view.cpp
@@ -1,27 +1,38 @@
-#include "Cell_class.h"
-#include "Array_class.h"
-#include "menu.h"
+#include <iostream>
+#include "Tablica.h"
+#include "Komorka.h"
 
-void Tablica::wyswietlanie(){
-			
-	int x, y;
-	interface_wysw(2);
-	cin >> x;
-				
-	while (x<0 || x>line){
-		interface_wysw(3);
-		cin >> x;
-	}
-	interface_wysw(1);
-	cin >> y;
-	while (y<0 || y>column){
-		interface_wysw(3);
-		cin >> y;
-	}
-	if(types[y]=="FLOAT"){
-		cout << cellArr[x][y].floatOut();
-	}
-	else{		
-		cout << cellArr[x][y].stringOut();
-	}
+#define SUCCESS 0
+
+using namespace std;
+
+int Tablica::wyswietl_tablice(Tablica arr)
+{
+    for (int i = 0; i < arr.rozmiarX; i++)
+    {
+        if (arr.tablica[i].pobierzTyp() == INT)
+            cout << " I ";
+        else if (arr.tablica[i].pobierzTyp() == STRING)
+            cout << " S ";
+    }
+
+    cout << endl;
+
+    for (int j = 0; j < arr.rozmiarY; j++)
+    {
+        for (int i = 0; i < arr.rozmiarX; i++)
+        {
+            cout << "|";
+            if (arr.tablica[i].pobierzKomorke(j)->pobierzTyp() == INT)
+                cout << arr.tablica[i].pobierzKomorke(j)->pobierzWartoscInt();
+            else
+                cout << arr.tablica[i].pobierzKomorke(j)->pobierzWartoscStr();
+
+            cout << "|";
+        }
+
+        cout << endl;
+    }
+
+    return SUCCESS;
 }
