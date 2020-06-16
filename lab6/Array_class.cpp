1b9a8071aa2c471707c8548bbe1d9e682d2ff685
019252752 <62552438+019252752@users.noreply.github.com>
2020-06-16 21:10:26 +0200

    Update Array_class.cpp

--- lab6/Array_class.cpp
+++ lab6/Array_class.cpp
@@ -1,249 +1,370 @@
-#include <iostream>
-#include "Array_class.h"
-#include "Cell_class.h"
+#include "tablica.hpp"
 
-int Tablica::zmien_rozmiar(Tablica *arr, int nowy_rozmiarX, int nowy_rozmiarY){
-
-	if(nowy_rozmiarX <= 0 || nowy_rozmiarY <= 0){
-		return ERR_INVALID_SIZE;
-	}
-	Tablica nowy_arr(nowy_rozmiarX);
-	nowy_arr.rozmiarY = nowy_rozmiarY;
-
-	kopiuj_zawartosc(*arr, nowy_arr);
-
-	delete[](*arr).tablica;
-
-	arr->tablica = nowy_arr.tablica;
-	arr->rozmiarX = nowy_arr.rozmiarX;
-	arr->rozmiarY = nowy_arr.rozmiarY;
-
-	return SUCCESS;
+Tablica::Tablica()
+{
+    x = 0;
+    y = 0;
+    typy_tablicy = nullptr;
+    tab = nullptr;
+}
+Tablica::Tablica(size_t nowy_x, size_t nowy_y, typy* nowe_typy)
+{
+    if (nowy_x == 0 || nowy_y == 0)
+    {
+        x = 0;
+        y = 0;
+        typy_tablicy = nullptr;
+        tab = nullptr;
+    }
+    else
+    {
+        x = nowy_x;
+        y = nowy_y;
+        typy_tablicy = new typy[x];
+        for (size_t i = 0; i < x; ++i)
+            typy_tablicy[i] = nowe_typy[i];
+        tab = new Komorka**[x];
+        for (size_t i = 0; i < x; ++i)
+        {
+            tab[i] = new Komorka*[y];
+            switch (typy_tablicy[i])
+            {
+                case typ_int:
+                    for (size_t j = 0; j < y; ++j)
+                    {
+                        tab[i][j] = new KomorkaInt();
+                    }
+                    break;
+                case typ_double:
+                    for (size_t j = 0; j < y; ++j)
+                    {
+                        tab[i][j] = new KomorkaDouble();
+                    }
+                    break;
+                case typ_string:
+                    for (size_t j = 0; j < y; ++j)
+                    {
+                        tab[i][j] = new KomorkaString();
+                    }
+                    break;
+            }
+        }
+    }
+}
+void Tablica::usun_stara()
+{
+    delete [] typy_tablicy;
+    for (size_t i = 0; i < x; ++i)
+    {
+        for (size_t j = 0; j < y; ++j)
+            delete tab[i][j];
+        delete [] tab[i];
+    }
+    delete [] tab;
+}
+void Tablica::skopiuj_nowa(const Tablica& t)
+{
+    if (t.x == 0 || t.y == 0)
+    {
+        x = 0;
+        y = 0;
+        typy_tablicy = nullptr;
+        tab = nullptr;
+    }
+    else
+    {
+        x = t.x;
+        y = t.y;
+        typy_tablicy = new typy[x];
+        for (size_t i = 0; i < x; ++i)
+            typy_tablicy[i] = t.typy_tablicy[i];
+        tab = new Komorka**[x];
+        for (size_t i = 0; i < x; ++i)
+        {
+            tab[i] = new Komorka*[y];
+            switch (typy_tablicy[i])
+            {
+                default:
+                case typ_int:
+                    for (size_t j = 0; j < y; ++j)
+                    {
+                        tab[i][j] = new KomorkaInt(*t.tab[i][j]);
+                    }
+                    break;
+                case typ_double:
+                    for (size_t j = 0; j < y; ++j)
+                    {
+                        tab[i][j] = new KomorkaDouble(*t.tab[i][j]);
+                    }
+                    break;
+                case typ_string:
+                    for (size_t j = 0; j < y; ++j)
+                    {
+                        tab[i][j] = new KomorkaString(*t.tab[i][j]);
+                    }
+                    break;
+            }
+        }
+    }
+}
+void Tablica::przenies_nowa(Tablica& t)
+{
+    x = t.x;
+    y = t.y;
+    typy_tablicy = t.typy_tablicy;
+    tab = t.tab;
+    t.x = 0;
+    t.y = 0;
+    t.typy_tablicy = nullptr;
+    t.tab = nullptr;
+}
+Tablica::Tablica(const Tablica& t)
+{
+    skopiuj_nowa(t);
+}
+Tablica::Tablica(Tablica&& t)
+{
+    przenies_nowa(t);
+}
+Tablica& Tablica::operator=(const Tablica& t)
+{
+    usun_stara();
+    skopiuj_nowa(t);
+    return *this;
+}
+Tablica& Tablica::operator=(Tablica&& t)
+{
+    usun_stara();
+    przenies_nowa(t);
+    return *this;
+}
+Tablica::~Tablica()
+{
+    usun_stara();
 }
 
-int Tablica::kopiuj_zawartosc(Tablica tab1, Tablica tab2){
-
-	for(int i = 0; i < tab1.rozmiarX; i++){
-		if(i >= tab2.rozmiarX){
-			break;
-        	}
-		for(int j = 0; j < tab1.rozmiarY; j++){
-			if(j < tab2.rozmiarY){
-				tab2.tablica[i].ustawKomorke(j, *(tab1.tablica[i].pobierzKomorke(j)));
-			}
-			else{
-				break;
-			}
-		}
-	}
-	return SUCCESS;
+size_t Tablica::zwroc_x() const
+{
+    return x;
+}
+size_t Tablica::zwroc_y() const
+{
+    return y;
+}
+typy* Tablica::zwroc_typy(typy* w) const
+{
+    typy* r = w == nullptr ? new typy[x] : w;
+    for (size_t i = 0; i < x; ++i)
+    {
+        r[i] = typy_tablicy[i];
+    }
+    return r;
 }
 
-int Tablica::aktualizuj_zawartoscINT(Tablica arr, int indeksX, int indeksY, int nowa_wartosc){
-
-	if(arr.tablica[indeksX].pobierzKomorke(indeksY)->pobierzTyp() != INT){
-		return ERR_INVALID_SIZE;
-	}
-
-	if(indeksX >= 0 && indeksX < arr.rozmiarX && indeksY >= 0 && indeksY < arr.rozmiarY){
-		arr.tablica[indeksX].pobierzKomorke(indeksY)->ustawWartoscInt(nowa_wartosc);
-		return SUCCESS;
-	}
-	return ERR_INVALID_SIZE;
+Komorka** Tablica::zwroc_kolumne(size_t k) const
+{
+    if (x == 0 || y == 0)
+        throw bledy(TABLICA_JEST_PUSTA);
+    if (k >= x)
+        throw bledy(INDEKS_POZA_TABLICA);
+    Komorka** r = new Komorka*[y];
+    for (size_t i = 0; i < y; ++i)
+    {
+        r[i] = tab[k][i];
+    }
+    return r;
+}
+Komorka** Tablica::zwroc_wiersz(size_t w) const
+{
+    if (x == 0 || y == 0)
+        throw bledy(TABLICA_JEST_PUSTA);
+    if (w >= y)
+        throw bledy(INDEKS_POZA_TABLICA);
+    Komorka** r = new Komorka*[x];
+    for (size_t i = 0; i < x; ++i)
+    {
+        r[i] = tab[i][w];
+    }
+    return r;
+}
+Komorka& Tablica::zwroc_komorke(size_t indeks_x, size_t indeks_y)
+{
+    if (x == 0 || y == 0)
+        throw bledy(TABLICA_JEST_PUSTA);
+    if (indeks_x >= x || indeks_y >= y)
+        throw bledy(INDEKS_POZA_TABLICA);
+    return *(tab[indeks_x][indeks_y]);
 }
 
-int Tablica::aktualizuj_zawartoscSTR(Tablica arr, int indeksX, int indeksY, std::string nowa_wartosc){
-
-	if(arr.tablica[indeksX].pobierzKomorke(indeksY)->pobierzTyp() != STRING){
-		return ERR_INVALID_SIZE;
-	}
-
-	if(indeksX >= 0 && indeksX < arr.rozmiarX && indeksY >= 0 && indeksY < arr.rozmiarY){
-		arr.tablica[indeksX].pobierzKomorke(indeksY)->ustawWartoscStr(nowa_wartosc);
-		return SUCCESS;
-	}
-	return ERR_INVALID_SIZE;
+void Tablica::wypisz(ostream& out, bool naglowek) const
+{
+    if (naglowek)
+    {
+        out << x << " " << y << endl;
+        for (size_t i = 0; i < x; ++i)
+        {
+            out << typy_tablicy[i] << " ";
+        }
+        out << endl;
+    }
+    for (size_t j = 0; j < y; ++j)
+    {
+        if (!naglowek)
+            out << "| ";
+        for (size_t i = 0; i < x; ++i)
+        {
+            out << *tab[i][j] << " ";
+            if (!naglowek)
+                out << "| ";
+        }
+        out << endl;
+    }
+}
+void Tablica::wczytaj(istream& in)
+{
+    usun_stara();
+    in >> x;
+    in >> y;
+    if (x == 0 || y == 0)
+    {
+        x = 0;
+        y = 0;
+        typy_tablicy = nullptr;
+        tab = nullptr;
+    }
+    else
+    {
+        typy_tablicy = new typy[x];
+        unsigned int temp_typ;
+        for (size_t i = 0; i < x; ++i)
+        {
+            in >> temp_typ;
+            typy_tablicy[i] = (typy)temp_typ;
+        }
+        tab = new Komorka**[x];
+        for (size_t i = 0; i < x; ++i)
+        {
+            tab[i] = new Komorka*[y];
+            switch (typy_tablicy[i])
+            {
+                default:
+                case typ_int:
+                    for (size_t j = 0; j < y; ++j)
+                    {
+                        tab[i][j] = new KomorkaInt();
+                    }
+                    break;
+                case typ_double:
+                    for (size_t j = 0; j < y; ++j)
+                    {
+                        tab[i][j] = new KomorkaDouble();
+                    }
+                    break;
+                case typ_string:
+                    for (size_t j = 0; j < y; ++j)
+                    {
+                        tab[i][j] = new KomorkaString();
+                    }
+                    break;
+            }
+        }
+        for (size_t j = 0; j < y; ++j)
+        {
+            for (size_t i = 0; i < x; ++i)
+            {
+                in >> *tab[i][j];
+            }
+        }
+    }
 }
 
-int Tablica::suma_kolumna(Tablica arr, int kolumna, int *sumaY){
-
-	if(kolumna >= 0 && kolumna < arr.rozmiarX){
-		if(arr.tablica[kolumna].pobierzTyp() != INT){
-			return ERR_INVALID_SIZE;
-		}
-		*sumaY = 0;
-
-		for(int i = 0; i < arr.rozmiarY; i++){
-			*sumaY += arr.tablica[kolumna].pobierzKomorke(i)->pobierzWartoscInt();
-		}
-	}
-	else{
-		return ERR_INVALID_SIZE;
-	}
-	return SUCCESS;
+void Tablica::zmien_rozmiar(size_t nowy_x, size_t nowy_y, typy* nowe_typy)
+{
+    if (nowy_x == 0 || nowy_y == 0)
+    {
+        usun_stara();
+        x = 0;
+        y = 0;
+        typy_tablicy = nullptr;
+        tab = nullptr;
+    }
+    else
+    {
+        typy* nowe_typy_tablicy = new typy[nowy_x];
+        for (size_t i = 0; i < nowy_x; ++i)
+            nowe_typy_tablicy[i] = nowe_typy[i];
+        Komorka*** nowe_tab = new Komorka**[nowy_x];
+        for (size_t i = 0; i < nowy_x; ++i)
+        {
+            nowe_tab[i] = new Komorka*[nowy_y];
+            switch (nowe_typy_tablicy[i])
+            {
+                case typ_int:
+                    for (size_t j = 0; j < nowy_y; ++j)
+                    {
+                        nowe_tab[i][j] = i < x && j < y ? new KomorkaInt(*tab[i][j]): new KomorkaInt();
+                    }
+                    break;
+                case typ_double:
+                    for (size_t j = 0; j < nowy_y; ++j)
+                    {
+                        nowe_tab[i][j] = i < x && j < y ? new KomorkaDouble(*tab[i][j]): new KomorkaDouble();
+                    }
+                    break;
+                case typ_string:
+                    for (size_t j = 0; j < nowy_y; ++j)
+                    {
+                        nowe_tab[i][j] = i < x && j < y ? new KomorkaString(*tab[i][j]): new KomorkaString();
+                    }
+                    break;
+            }
+        }
+        usun_stara();
+        x = nowy_x;
+        y = nowy_y;
+        typy_tablicy = nowe_typy_tablicy;
+        tab = nowe_tab;
+    }
 }
 
-int Tablica::suma_wiersz(Tablica arr, int wiersz, int *sumaX){
-	if(wiersz >= 0 && wiersz < arr.rozmiarY){
-		*sumaX = 0;
-
-        	for(int j = 0; j < arr.rozmiarX; j++){
-			if(arr.tablica[j].pobierzKomorke(wiersz)->pobierzTyp() != INT){
-				continue;
-			}
-		*sumaX += arr.tablica[j].pobierzKomorke(wiersz)->pobierzWartoscInt();
-		}
-	}
-	else{
-		return ERR_INVALID_SIZE;
-	}
-	return SUCCESS;
+double Tablica::policz_min(Komorka** t, size_t n)
+{
+    double r = t[0] -> wartosc_statystyczna();
+    double temp;
+    for (size_t i = 1; i < n; ++i)
+    {
+        temp = t[i] -> wartosc_statystyczna();
+        if (temp < r)
+        {
+            r = temp;
+        }
+    }
+    return r;
 }
-
-int Tablica::minimumY(Tablica arr, int wiersz, int *najmniejszaX){
-
-	if(wiersz >= 0 && wiersz < arr.rozmiarY){
-		*najmniejszaX = 0;
-
-		for(int i = 0; i < arr.rozmiarX; i++){
-			if(arr.tablica[i].pobierzKomorke(wiersz)->pobierzTyp() != INT){
-				continue;
-			}
-			*najmniejszaX = arr.tablica[i].pobierzKomorke(wiersz)->pobierzWartoscInt();
-
-			if(arr.tablica[i].pobierzKomorke(wiersz)->pobierzWartoscInt() < *najmniejszaX){
-				*najmniejszaX = arr.tablica[i].pobierzKomorke(wiersz)->pobierzWartoscInt();
-			}
-		}
-	}
-	else{
-		return ERR_INVALID_SIZE;
-	}
-	return SUCCESS;
+double Tablica::policz_max(Komorka** t, size_t n)
+{
+    double r = t[0] -> wartosc_statystyczna();
+    double temp;
+    for (size_t i = 1; i < n; ++i)
+    {
+        temp = t[i] -> wartosc_statystyczna();
+        if (temp > r)
+        {
+            r = temp;
+        }
+    }
+    return r;
 }
-
-int Tablica::minimumX(Tablica arr, int kolumna, int *najmniejszaY){
-
-	if(kolumna >= 0 && kolumna < arr.rozmiarX){
-		if(arr.tablica[kolumna].pobierzTyp() != INT){
-			return ERR_INVALID_SIZE;
-		}
-		*najmniejszaY = 0;
-
-		for(int j = 0; j < arr.rozmiarY; j++){
-			*najmniejszaY = arr.tablica[kolumna].pobierzKomorke(j)->pobierzWartoscInt();
-
-			if(arr.tablica[kolumna].pobierzKomorke(j)->pobierzWartoscInt() < *najmniejszaY){
-				*najmniejszaY = arr.tablica[kolumna].pobierzKomorke(j)->pobierzWartoscInt();
-			}
-		}
-	}
-	else{
-		return ERR_INVALID_SIZE;
-	}
-	return SUCCESS;
+double Tablica::policz_sum(Komorka** t, size_t n)
+{
+    double r = 0.0;
+    for (size_t i = 0; i < n; ++i)
+    {
+        r += t[i] -> wartosc_statystyczna();
+    }
+    return r;
 }
-
-int Tablica::maximumX(Tablica arr, int kolumna, int *najwiekszaX){
-
-	if (kolumna >= 0 && kolumna < arr.rozmiarX){
-		if(arr.tablica[kolumna].pobierzTyp() != INT){
-			return ERR_INVALID_SIZE;
-		}
-		*najwiekszaX = 0;
-
-		for(int j = 0; j < arr.rozmiarY; j++){
-			*najwiekszaX = arr.tablica[kolumna].pobierzKomorke(j)->pobierzWartoscInt();
-
-			if(arr.tablica[kolumna].pobierzKomorke(j)->pobierzWartoscInt() > *najwiekszaX){
-				*najwiekszaX = arr.tablica[kolumna].pobierzKomorke(j)->pobierzWartoscInt();
-			}
-		}
-	}
-	else{
-		return ERR_INVALID_SIZE;
-	}
-	return SUCCESS;
-}
-
-int Tablica::maximumY(Tablica arr, int wiersz, int *najwiekszaY){
-
-	if(wiersz >= 0 && wiersz < arr.rozmiarY){
-		*najwiekszaY = 0;
-
-		for(int i = 0; i < arr.rozmiarX; i++){
-			if(arr.tablica[i].pobierzKomorke(wiersz)->pobierzTyp() != INT){
-				continue;
-			}
-			*najwiekszaY = arr.tablica[i].pobierzKomorke(wiersz)->pobierzWartoscInt();
-
-			if(arr.tablica[i].pobierzKomorke(wiersz)->pobierzWartoscInt() > *najwiekszaY){
-				*najwiekszaY = arr.tablica[i].pobierzKomorke(wiersz)->pobierzWartoscInt();
-			}
-		}
-	}
-	else{
-		return ERR_INVALID_SIZE;
-	}
-	return SUCCESS;
-}
-
-int Tablica::srednia_wiersz(Tablica arr, int wiersz, double *sredniaX, int *sumaX){
-
-	if(wiersz >= 0 && wiersz < arr.rozmiarY){
-		*sredniaX = 0;
-		*sumaX = 0;
-        
-		for(int i = 0; i < arr.rozmiarX; i++){
-			if(arr.tablica[i].pobierzKomorke(wiersz)->pobierzTyp() != INT){
-				continue;
-			}
-			*sumaX += arr.tablica[i].pobierzKomorke(wiersz)->pobierzWartoscInt();
-		}
-		*sredniaX = ((double)*sumaX / arr.rozmiarX);
-	}
-	else{
-		return ERR_INVALID_SIZE;
-	}
-	return SUCCESS;
-}
-
-int Tablica::srednia_kolumna(Tablica arr, int kolumna, double *sredniaY, int *sumaY){
-
-	if(kolumna >= 0 && kolumna < arr.rozmiarX){
-		if(arr.tablica[kolumna].pobierzTyp() != INT){
-			return ERR_INVALID_SIZE;
-		}
-		*sredniaY = 0;
-		*sumaY = 0;
-		for(int j = 0; j < arr.rozmiarY; j++){
-			*sumaY += arr.tablica[kolumna].pobierzKomorke(j)->pobierzWartoscInt();
-		}
-		*sredniaY = ((double)*sumaY / arr.rozmiarY);
-	}
-	else{
-		return ERR_INVALID_SIZE;
-	}
-	return SUCCESS;
-}
-
-int Tablica::zmienTyp(int typ, int zmienianaKolumna){
-
-	if(zmienianaKolumna >= 0 && zmienianaKolumna < rozmiarX){
-		if(typ == INT && tablica[zmienianaKolumna].pobierzTyp() != INT){
-			KolumnaInt *nowaKolumna = new KolumnaInt();
-			tablica[zmienianaKolumna] = *nowaKolumna;
-		}
-		else if(typ == STRING && tablica[zmienianaKolumna].pobierzTyp() != STRING){
-			KolumnaStr *nowaKolumna = new KolumnaStr();
-			tablica[zmienianaKolumna] = *nowaKolumna;
-		}
-		else{
-			return ERR_INVALID_SIZE;
-		}
-	}
-	else{
-		return ERR_INVALID_SIZE;
-	}
-	return SUCCESS;
+double Tablica::policz_avg(Komorka** t, size_t n)
+{
+    return Tablica::policz_sum(t, n) / (double)n;
 }
