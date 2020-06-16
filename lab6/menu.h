cabeb13710829f3c828b99711b8abbb33eb956b7
019252752 <62552438+019252752@users.noreply.github.com>
2020-06-16 21:14:39 +0200

    Update menu.h

--- lab6/menu.h
+++ lab6/menu.h
@@ -1,2 +1,13 @@
-#pragma once
-int menu();
+#ifndef menu_hpp
+#define menu_hpp
+
+#include <iostream>
+#include <fstream>
+#include "menu.hpp"
+#include "tablica.hpp"
+using namespace std;
+
+void pisz_bledy(ostream& out, bledy b);
+void menu();
+
+#endif /* menu_hpp */
